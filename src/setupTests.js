415be4365e5f8185e1ca7ea1d0ae9a61aa5ec098
Glenn Dwiyatcita <glenn.dwiyatcita@sap.com>
2024-11-30 14:19:56 +0100

    doc: add ref comment for "ReferenceError: TextEncoder is not defined" workaround

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,6 +4,10 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 
+/**
+ * workaround for "ReferenceError: TextEncoder is not defined"
+ * @see https://github.com/remix-run/react-router/issues/12363#issuecomment-2496226528
+ */
 if (!globalThis.TextEncoder || !globalThis.TextDecoder) {
   const { TextDecoder, TextEncoder } = require('node:util');
   globalThis.TextEncoder = TextEncoder;
