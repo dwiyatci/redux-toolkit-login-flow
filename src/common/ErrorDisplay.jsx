d5f716433baa8590cdb0750bddf3e93441f5490e
Glenn Dwiyatcita <dwiyatci@mail.com>
2025-08-02 22:37:06 +0200

    Revert "⚡︎ feat: migrate from CRA to Vite (#52)"
    This reverts commit e6740cae1d6ac0ff4ff0096d9b135d22806b1828.

--- src/common/ErrorDisplay.jsx
+++ src/common/ErrorDisplay.jsx
@@ -1,11 +1,12 @@
 // @flow
 
-import * as React from 'react';
+import React from 'react';
+import type { Node, ComponentType } from 'react';
 
 type Props = {
   message: string,
 };
-function ErrorDisplay({ message }: Props): React.Node {
+function ErrorDisplay({ message }: Props): Node {
   return (
     <p className="error-block" style={{ color: 'red' }}>
       {message}
@@ -15,4 +16,4 @@ function ErrorDisplay({ message }: Props): React.Node {
 
 ErrorDisplay.displayName = 'ErrorDisplay';
 
-export const MemoizedErrorDisplay: React.ComponentType<Props> = React.memo(ErrorDisplay);
+export const MemoizedErrorDisplay: ComponentType<Props> = React.memo(ErrorDisplay);
